500ce1b679707ce50da1fac9e36ee71a79a2a409
DroidFreak32 <rushabshah32@gmail.com>
2022-02-12 04:45:40 +0530

    Add more features
    * Initial Whitelist/Blacklist support
    * Playlist generation support
    * Add m3u helpers
    * Allow import/export of whitelist
    * Incremental db update support

--- spotifylesystem-sync.py
+++ spotifylesystem-sync.py
@@ -1,4 +1,4 @@
-from db_ops import complete_sync, partial_sync, generate_playlist
+from db_ops import sync_fs_to_db, export_altColumns, import_altColumns, partial_sync, generate_playlist
 
 from common import cls
 print("outside common")
@@ -9,7 +9,8 @@ def menu():
     menu_output = ("1) Complete Sync filesystem to Local DB\n"
                    "2) Update modified files in DB\n"
                    "3) Generate spotify playlists locally\n"
-                   "4) Dump missing playlist tracks\n"
+                   "4) Export whitelist & blacklist\n"
+                   "5) Import whitelist & blacklist\n"
                    "Q - Quit\n"
                    "Enter your choice: ")
     choice = input(menu_output)
@@ -21,15 +22,18 @@ def main():
     while True:
         choice = menu()
         if choice == '1':
-            complete_sync()
+            sync_fs_to_db()
         elif choice == '2':
             partial_sync()
         elif choice == '3':
             generate_playlist()
         elif choice == '4':
-            generate_playlist()
+            export_altColumns()
+        elif choice == '5':
+            import_altColumns()
         else:
             break
+        input("Press enter to go back to the main menu")
     print("Received config")
 
 
