746380b7ab889454ea054068d9b060601b218bdd
DroidFreak32 <rushabshah32@gmail.com>
2022-09-11 23:36:36 +0530

    db_ops: search: Implement options to abort, save or bypass album when matching tracks manually. * Make the prompts case-insensitive as well
    Changes to be committed:
        modified:   db_ops.py

--- db_ops.py
+++ db_ops.py
@@ -316,30 +316,56 @@ def search_track_in_db(track_metadata=None, album_artist=None):
                 """
                 We don't have an exact match, so prompt user to verify and update alternate / blacklist tags in DB.
                 """
-                print(f"\nSpotify URL:"
-                      f"\n{track_metadata['SPOTIFY']}"
+                print(f"\nSpotify URL: {track_metadata['SPOTIFY']}"
                       f"\nSpotify / DB Title:"
-                      f"\n{bcolors.OKBLUE}{track_metadata['TITLE']}{bcolors.ENDC} / {bcolors.OKCYAN}{row.TITLE}{bcolors.ENDC}"
+                      f"\n{bcolors.OKGREEN}{track_metadata['TITLE']}{bcolors.ENDC} / {bcolors.OKCYAN}{row.TITLE}{bcolors.ENDC}"
                       f"\n\nPATH {row.PATH}"
                       f"\n\nAre these the same?")
-                answer = input("Y/N/Q: ")
+                message = "(Y)es, this is an alternate title." \
+                          "\n(A)dd album to whitelist as well." \
+                          "\n(N)o, blacklist this title from future matches." \
+                          "\n(O)pen the file to check" \
+                          "\n(S)ave current changes and return to main menu." \
+                          "\n(Q)uit to main menu & Discard all changes: "
+                answer = input(message)[0].casefold()
+
                 if answer == 'Y' or answer == 'y':
                     print(f"\n*** Ignoring Title ***")
                     add_to_alt_title(db_row=row, track_metadata=track_metadata)
                     # Force skip next section
                     bypass_title = True
-                elif answer == 'q' or answer == 'Q':
-                    return 999
-                elif answer == 'n' or answer == 'N':
+                    if answer == 'a':
+                        bypass_album = True
+                        # Explicitly set answer = y to force add album to whitelist in the next _if_ section
+                        answer = 'y'
+
+                elif answer.casefold() == 'q':
+                    return 9
+                elif answer.casefold() == 's':
+                    return 99
+                elif answer.casefold() == 'n':
                     add_to_black_title(
                         db_row=row, track_metadata=track_metadata)
                 else:
+                    print("Invalid input, skipping track.")
                     continue
 
+            ##########################
+            # Album Matching section #
+            ##########################
             if bypass_title or db_title == spotify_title or \
                     is_title_in_alt_title(db_row=row, track_metadata=track_metadata):
+
+                # We cannot use fuzz as we want to ensure the album is exactly the same or prompt the user!
                 if is_item_in_db_column(row.ALBUM, track_metadata['ALBUM']) \
                         or is_album_in_alt_album(db_row=row, track_metadata=track_metadata):
+                    """
+                    TODO: If there are multiple paths, use the path index corresponding to the matching Album index.
+                    For ex, spotify's "My Propeller" belongs to DB Albums [My Propeller, Humbug]
+                    So return the path corresponding to the matched album.
+                    Since they are the same track, it *probably* won't be queried again after its matched.
+                    """
+
                     result.append({
                         'ALBUMARTIST': album_artist.ALBUMARTIST,
                         'PATH': str_to_list(row.PATH),
@@ -351,20 +377,25 @@ def search_track_in_db(track_metadata=None, album_artist=None):
                         # If subsequent iterations do not get this track then it will be a part of unmatched list.
                         continue
 
-                    print(f"\nSpotify URL:"
-                          f"\n{track_metadata['SPOTIFY']}"
+                    print(f"\nSpotify URL: {track_metadata['SPOTIFY']}"
                           f"\nSpotify / DB Album:"
-                          f"\n{bcolors.OKBLUE}{track_metadata['ALBUM']}{bcolors.ENDC} / {bcolors.OKCYAN}{row.ALBUM}{bcolors.ENDC}"
+                          f"\n{bcolors.OKGREEN}{track_metadata['ALBUM']}{bcolors.ENDC} / {bcolors.OKCYAN}{row.ALBUM}{bcolors.ENDC}"
                           f"\n\nPATH {row.PATH}"
                           f"\nAre these the same?")
-                    answer = input("Y/N/A (All Album)/Q: ")
+
+                    message = "(Y)es, this is an alternate Album." \
+                              "\n(A)llow all tracks from this album to match Spotify's Album." \
+                              "\n(N)o, blacklist this Album from future matches." \
+                              "\n(S)ave current changes and return to main menu." \
+                              "\nDiscard all changes & (Q)uit to main menu: "
+                    answer = input(message).casefold()[0]
 
                     if answer == 'n' or answer == 'N':
                         add_to_black_album(row, track_metadata)
                         continue
 
-                    if answer == 'Y' or answer == 'y' or answer == 'A':
-                        if answer == 'A':
+                    if answer == 'y' or answer == 'a':
+                        if answer == 'a':
                             query = Music.select().where(
                                 (Music.ALBUMARTIST == row.ALBUMARTIST) & (Music.ALBUM == row.ALBUM))
                             for row2 in query:
@@ -376,9 +407,12 @@ def search_track_in_db(track_metadata=None, album_artist=None):
                             'PATH': str_to_list(row.PATH),
                             'STREAMHASH': row.STREAMHASH
                         })
-                    elif answer == 'q' or answer == 'Q':
-                        return 999
+                    elif answer == 's':
+                        return 9
+                    elif answer == 'q':
+                        return 99
                     else:
+                        print("Invalid input, skipping track.")
                         continue
                 # if spotify_album_artist not in result.keys():
                 #     result[spotify_album_artist] = []
@@ -530,6 +564,11 @@ def generate_local_playlist(all_saved_tracks=False):
     """
     TODO: Instead of scanning each track, merge th AlbumArtist and just have 1 lookup per AA in DB
     """
+    # Flags to quit generating playlists.
+    skip_generation_and_save = False
+    abort_abort = False
+
+    # Copy DB in memory to avoid mistakes being reflected into the main DB.
     master = CSqliteExtDatabase(db_path)
     master.backup(db)
     if not all_saved_tracks:
@@ -567,12 +606,18 @@ def generate_local_playlist(all_saved_tracks=False):
             continue
 
         for playlist_track in spotify_playlist_tracks_merged[spotify_album_artist]:
+            skip_generation_and_save = False
+            abort_abort = False
             offset += 1
             print(
                 f"Querying DB for tracks: {offset} / {spotify_playlist_track_total}", end="\r")
             result = search_track_in_db(
                 track_metadata=playlist_track, album_artist=album_artist)
-            if result == 999:
+            if result == 9:
+                skip_generation_and_save = True
+                break
+            elif result == 99:
+                abort_abort = True
                 break
             if len(result) > 1:
                 print(
@@ -588,20 +633,27 @@ def generate_local_playlist(all_saved_tracks=False):
                 continue
             matched_list += result
             if isinstance(str_to_list(result[0]['PATH']), list):
+
                 # Use the 1st PATH. TODO: Make this more accurate by checking Album
+                # TAG: cd213e2f
                 result[0]['PATH'] = result[0]['PATH'][0]
             matched_paths.append(result[0]['PATH'])
+        if abort_abort:
+            print(f"{bcolors.WARNING}Aborting!{bcolors.ENDC}")
+            return
+        elif skip_generation_and_save:
+            break
 
     unmatched_dict = list2dictmerge(deepcopy(unmatched_list))
     matched_dict = list2dictmerge(deepcopy(matched_list))
 
     print(f"\n{len(matched_list)}/{spotify_playlist_track_total} tracks Matched. ")
 
-    if input("Do you want to generate an m3u file for the matched songs?\nY/N: ") == 'Y':
+    if input("Do you want to generate an m3u file for the matched songs?\nY/N: ")[0].casefold() == 'y':
         generate_m3u(playlist_name=spotify_playlist_name,
                      track_paths=matched_paths)
     if len(unmatched_track_ids) > 0:
-        if input("Do you want to generate a new spotify playlist for the UNMATCHED songs?\nY/N: ") == 'Y':
+        if input("Do you want to generate a new spotify playlist for the UNMATCHED songs?\nY/N: ")[0].casefold() == 'Y':
             spotify_ops.generate_missing_track_playlist(unmatched_track_ids=unmatched_track_ids,
                                                         playlist_name=spotify_playlist_name)
 
